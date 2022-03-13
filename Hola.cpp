d0d7506b5a0252c47d9a7f0fd0f421d115745520
Ferran <ferran.de.miguel@estudiantat.upc.edu>
2022-03-13 18:10:10 +0100

    Va

---
+++ Hola.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+struct A{
+    int a0;
+    float b0;
+};
+int main(){
+    struct A Primer;
+    std::cout<<"Holdsbha"<<std::endl;
+    std::cout<<"Hola1"<<std::endl;
+    std::cout<<"Hola546654654"<<std::endl;
+    std::cout<<"Hola3"<<std::endl;
+    Primer.a0=12;
+    Primer.b0=32;
+}
