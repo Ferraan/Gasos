7d191e24138bc0c0af1b7eed84e8e2e9e5c17a2d
Ferran <ferran.de.miguel@estudiantat.upc.edu>
2022-03-15 21:12:05 +0100

    Arrays part 2

--- TDMA/TDMA.cpp
+++ TDMA/TDMA.cpp
@@ -1,74 +1,81 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 using namespace std;
-int main(){
-    double pi = 2*acos(0.0);
-    double SE=0;
-    double SW=0;
-    double  Rint=2;
-    double  Rext=10;
-    double  ef=0.5;
-    double  lambda=300;
-    double  Twall=500;
-    double  Text=25;
-    double  alphaext=30;
-    double  alphaend=20;
-    const int n=10;
-    double  deltaR=(Rext-Rint)/n;
-    double T[n+1];
-    double aP[n+2];
-    double aW[n+2];
-    double aE[n+2];;
-    double bP[n+2];;
-    double rP[n+2];;
-    double A[n+2];;
-    double P[n+2];;
-    double R[n+2];;
-    for (int i=1; i<=n; i++){
-        T.push_back(1);
-        aW.push_back(1);
-        aP.push_back(1);
-        aE.push_back(1);
-        bP.push_back(1);
-        rP.push_back(1);
+double re(double rP, double deltaR);
+double rw(double rP, double deltaR);
+int main()
+{
+    double pi = 2 * acos(0.0);
+    double Rint = 2;
+    double Rext = 10;
+    double ef = 0.5;
+    double lambda = 300;
+    double Twall = 500;
+    double Text = 25;
+    double alphaext = 30;
+    double alphaend = 20;
+    const int n = 10;
+    double deltaR = (Rext - Rint) / n;
+    double T[n + 4];
+    double aP[n + 2];
+    double aW[n + 2];
+    double aE[n + 2];
+    double bP[n + 2];
+    double rP[n + 2];
+    double AP[n+2];
+    double Se[n+2];
+    double Sw[n+2];
+    double P[n + 4];
+    double R[n + 4];
+    aP[1] = 1;
+    aW[1] = 0;
+    aE[1] = 0;
+    bP[1] = Twall;
+    //Posicio nodes "reals"
+    rP[0] = Rint;
+    rP[1] = rP[0] + deltaR / 2;
+    for (int i = 2; i <= n; i++)
+    {
+        rP[i] = rP[i - 1] + deltaR;
     }
-    rP[0]=Rint;
-    aP[0]=1;
+    rP[n + 1] = Rext;
+    //Ap
+    AP[0]=0;
+    for (int i = 1; i < n+1; i++)
+    {
+        AP[i]=2*pi*(pow(re(rP[i],deltaR),2)-pow(rw(rP[i],deltaR),2));
+    }
+    AP[n+1]=0;
+    
+    Sw[0]=0;
+    Se[n+1]=0;
+    for (int i = 1; i < n+2; i++)
+    {
+        Sw[i]=2*pi*(rP[i]-deltaR/2)*ef;
+        Se[i]=2*pi*(rP[i]+deltaR/2)*ef;
+    }
     aW[0]=0;
     aE[0]=0;
     bP[0]=Twall;
-    for (int i = 1; i < n; i++){
-        rP[i]=rP[i-1]+deltaR;
-    }
-    rP.push_back(Rext);
-    A.push_back(0);
-    for (int i = 1; i < n; i++){
-        A.push_back(2*pi*(pow(rP[i+1]-(deltaR/2),2)-pow(rP[i-1]+(deltaR/2),2)));
-        SE=2*pi*(rP[i+1]-deltaR/2);
-        SW=2*pi*(rP[i-1]+deltaR/2);
-        aW[i]=lambda*SW/deltaR;
-        aE[i]=lambda*SE/deltaR;
-        aP[i]=aW[i]+aE[i]+alphaext*A[i];
-        bP[i]=alphaext+Text*A[i];
-    }
-    aW.push_back(lambda*pow(rP[size(rP)-1],2)*pi/deltaR);
-    bP.push_back(alphaend*Text+2*pi*Rext*ef+alphaext*Text*(pow(Rext,2)-pow(Rext-deltaR/2,2))*pi);
-    aE.push_back(0);
-    aP.push_back(aW[size(aW)-1]+alphaend*2*pi*Rext*ef+alphaext*(pow(Rext,2)-pow(Rext-deltaR/2,2))*pi);
-    P.push_back(aE[0]/aP[0]);
-    R.push_back(bP[0]/aP[0]);
-    for (int i = 1; i <= n+1; i++)
+    aP[0]=1;
+    for (int i = 1; i < n+1; i++)
     {
-        P.push_back(aE[i]/(aP[i]-aW[i]*P[i-1]));
-        R.push_back((bP[i]+aW[i]*R[i-1])/(aP[i]-aW[i]*P[i-1]));
+        aW[i]=(lambda*Sw[i])/(deltaR);
+        aE[i]=(lambda*Se[i])/(deltaR);
+        aP[i]=aE[i]+aW[i]+alphaext*AP[i];
+        bP[i]=alphaext*Text*AP[i];
     }
-    T.push_back(0);
-    for (int i = n+1; i > 0; i--)
-    {
-        T[i]=P[i]*T[i+1]+R[i];
-        cout<<T[i]<<endl;
-    }
-    
-    
+    aE[n+1]=0;
+    aW[n+1]=lambda/deltaR;
+    aP[n+1]=aW[n+1]+alphaend;
+    bP[n+1]=alphaend*Text;
+
+}
+
+
+double re(double rP, double deltaR){
+        return(rP+deltaR/2);
+}
+double rw(double rP, double deltaR){
+    return(rP-deltaR/2);
 }
