f28ed9ee7382fbcc1f237a43b8befb7338732a74
Ferran de Miguel <ferran.de.miguel@estudiantat.upc.edu>
2022-05-28 20:50:53 +0200

    Tub anular fet

--- Treball/Treball.cpp
+++ Treball/Treball.cpp
@@ -3,22 +3,24 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <chrono>
 using namespace std;
+auto start = chrono::high_resolution_clock::now();
 class fluid
 {
     private:
     double Reynolds, Prandt, Nusselt;
     public:
     double Alfa_i, fregament, viscositat , densitat ,conductivitat ,cp;
-    void Calcul_Coeficients(double mu, double cp, double lambda, double D, double v, double rho, double rugositat_relativa, double x);
+    void Calcul_Coeficients(double mu, double cp, double lambda, double D, double v, double rho, double rugositat_relativa);
     void Propietats_termofisiquesH2(double T, double P,double Rgas);
     void Propietats_termofisiquesO2(double T, double P,double Rgas);
     void Propietats_termofisiquesmescla(double T, double P,double Rgas,double cpA,double cpB,double fmolA,double fmolB, double muA, double muB, double conductivitatA, double conductivitatB);
+    void Calcul_Coeficients_anular(double mu, double cp, double lambda, double D, double v, double rho, double rugositat_relativa);
 };
 
 
-
-const int n = 5; //Volums de control del fluid, n+1 nodes
+const int n = 500; //Volums de control del fluid, n+1 nodes
 const double delta = 1e-10;
 const double pi = 2 * acos(0.0);
 const double Runiversal=8.3144621;
@@ -27,23 +29,21 @@ const double massa_molarH2=2e-3, massa_molarO2=32e-3;//Kg/mol
 int main(){
     std::cout.precision(15);
     std::scientific;
-    double L=0.5, Di=0.02, Do=0.023, Dm=0.03, DOut=0.033;
-    double Tin1=1000, pin1=161e+5, Tin3=100, cabalin3=100, pin3=150e+5, Text=300;
+    double L=0.5, Di=0.02, Do=0.023, Dm=0.033, DOut=0.035;
+    double Tin1=1000, pin1=161e+5, Tin3=100, cabalin3=0.005, pin3=150e+5, Text=300;
     double cabalin1H2=0.005, cabalin1O2=0.05, cabalin1Tot=cabalin1H2+cabalin1O2;
     double Ttub2_inic=800, Ttub4_inic=500;
     double rugositat2in=0.0002, rugositat2ext=0.0003, rugositat4in=0.0004, rugositat4ext=0.0002;
     
     //Calculs previs
-    double Deltax=L/n;
-    double S1=pi*Di*Di/4, S3=pi*pow(Dm-Do,2)/4, Sl2int=pi*Di*Deltax;
-    cout<<S1<<endl;
+    double Deltax=L/n, Dh=Dm-Do;
+    double S1=pi*Di*Di/4, S3=pi*pow(Dm-Do,2)/4, Sl2int=pi*Di*Deltax, Sl2out=pi*Do*Deltax, Sl4in=pi*Dm*Deltax;
     double molsH2=cabalin1H2/massa_molarH2, molsO2=cabalin1O2/massa_molarO2;
     double fraccio_molarH2=molsH2/(molsH2+molsO2), fraccio_molarO2=molsO2/(molsH2+molsO2), massa_molar_cambra=fraccio_molarH2*massa_molarH2+fraccio_molarO2*massa_molarO2;
-    cout<<fraccio_molarH2<<"  "<<fraccio_molarO2<<"  "<<massa_molar_cambra<<endl;
     double Rgas_cambra=Runiversal/massa_molar_cambra, Rhidrogen=Runiversal/massa_molarH2, Roxigen=Runiversal/massa_molarO2;
     double rhoin1=pin1/(Tin1*Rgas_cambra), rhoin3=pin3/(Tin3*Rhidrogen);
     double vin1=cabalin1Tot/(S1*rhoin1), vin3=cabalin3/(S3*rhoin3);
-    std::vector<double> x1(n+1,0), v1(n+1,0), T1(n+1,0), p1(n+1,0), rho1(n+1,0), v3(n+1,0),T3(n+1,0), p3(n+1,0), rho3(n+1,0), T2(n,Ttub2_inic), T4(n,Ttub4_inic), alfa1(n,0);
+    std::vector<double> x1(n+1,0), v1(n+1,0), T1(n+1,0), p1(n+1,0), rho1(n+1,0), v3(n+1,0),T3(n+1,0), p3(n+1,0), rho3(n+1,0), T2(n,Ttub2_inic), T4(n,Ttub4_inic), alfa1(n,0), alfa3(n,0);
     x1[0]=0;
     for (int i = 1; i < n+1; i++)
     {
@@ -51,15 +51,13 @@ int main(){
     }
     
    
-    //Zona 1
+    //Zona 1 i 3
     v1[0]=vin1; T1[0]=Tin1; p1[0]=pin1; rho1[0]=rhoin1;
+    v3[0]=vin3; T3[0]=Tin3; p3[0]=pin3; rho3[0]=rhoin3;
     fluid H2cambra, O2cambra, H2ext, mescla_cambra;
    
     for (int i=1; i<n+1; i++){
-        v1[i]=v1[i-1]; 
-        p1[i]=p1[i-1]; 
-        T1[i]=T1[i-1]; 
-        rho1[i]=rho1[i-1];
+        v1[i]=v1[i-1]; p1[i]=p1[i-1]; T1[i]=T1[i-1]; rho1[i]=rho1[i-1];
         
         double error=1.0;
        
@@ -69,7 +67,7 @@ int main(){
             H2cambra.Propietats_termofisiquesH2(Ti,Pi,Rhidrogen); 
             O2cambra.Propietats_termofisiquesO2(Ti,Pi,Roxigen);
             mescla_cambra.Propietats_termofisiquesmescla(Ti,Pi,Rgas_cambra,H2cambra.cp,O2cambra.cp,fraccio_molarH2,fraccio_molarO2,H2cambra.viscositat,O2cambra.viscositat,H2cambra.conductivitat,H2cambra.conductivitat);
-            mescla_cambra.Calcul_Coeficients(mescla_cambra.viscositat,mescla_cambra.cp,mescla_cambra.conductivitat,Di,vi,mescla_cambra.densitat,rugositat2in,x1[i]);
+            mescla_cambra.Calcul_Coeficients(mescla_cambra.viscositat,mescla_cambra.cp,mescla_cambra.conductivitat,Di,vi,mescla_cambra.densitat,rugositat2in);
             alfa1[i-1]=mescla_cambra.Alfa_i;
             p1[i]=-cabalin1Tot*(v1[i]-v1[i-1])/S1+p1[i-1]-mescla_cambra.fregament*rhoi*pow(vi,2)/(2*S1)*pi*Di*Deltax;
             double cA=cabalin1Tot*(pow(v1[i],2)-pow(v1[i-1],2))/2.0, cB=cabalin1Tot*mescla_cambra.cp+mescla_cambra.Alfa_i*Sl2int/2; //Cal canviar el cp a la mitjana
@@ -81,15 +79,38 @@ int main(){
             error=max(abs(vold-v1[i]),error);
             error=max(abs(rho1[i]-rhoold),error);
         }
-        
+        //Zona 3
+        v3[i]=v3[i-1]; p3[i]=p3[i-1]; T3[i]=T3[i-1]; rho3[i]=rho3[i-1];
+        error=1;
+        while (error>delta){     
+            double pold=p3[i], Told=T3[i], vold=v3[i], rhoold=rho3[i]; 
+            double Ti=(T3[i]+T3[i-1])/2, Pi=(p3[i-1]+p3[i])/2,vi=(v3[i]+v3[i-1])/2,rhoi=(rho3[i]+rho3[i-1])/2;
+            H2ext.Propietats_termofisiquesH2(Ti,Pi,Rhidrogen);    
+            H2ext.Calcul_Coeficients(H2ext.viscositat,H2ext.cp,H2ext.conductivitat,Dh,vi,H2ext.densitat,rugositat2ext);
+            alfa3[i-1]=H2ext.Alfa_i;
+            p3[i]=-cabalin3*(v3[i]-v3[i-1])/S3+p3[i-1]-H2ext.fregament*rhoi*pow(vi,2)/(2*S3)*pi*Dh*Deltax;
+            double cA=cabalin1Tot*(pow(v1[i],2)-pow(v1[i-1],2))/2.0, cB=cabalin3*H2ext.cp+H2ext.Alfa_i/2 *(Sl2out+Sl4in);
+            T3[i]=(T3[i-1]*(cabalin3*H2ext.cp-H2ext.Alfa_i/2*(Sl2out+Sl4in))-cA+H2ext.Alfa_i*(T2[i-1]*Sl2out+T4[i-1]*Sl4in))/cB;
+            rho3[i]=p3[i]/(T3[i]*Rhidrogen);
+            v3[i]=cabalin3/(rho3[i]*S3);
+            error=max(abs(p3[i]-pold), abs(Told-T3[i]));
+            error=max(abs(vold-v3[i]),error);
+            error=max(abs(rho3[i]-rhoold),error);
+        }
         
     }
+    
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    cout<<"Temps execucio (s)" <<static_cast<float>(duration.count())/1000000 << endl;
+    
+    
     ofstream fout;
     fout.open("Treball.csv");
-    fout<<"i"<<","<<"x1[i]"<<","<<"T1[i]"<<","<<"P1[i]"<<","<<"v1[i]"<<","<<"rho1[i]"<<","<<"alfa[i]"<<","<<"[i]"<<endl; //alfa, T2,T4[n]=0 perque no hi ha nodes
+    fout<<"i"<<","<<"x1[i]"<<","<<"T1[i]"<<","<<"P1[i]"<<","<<"v1[i]"<<","<<"rho1[i]"<<","<<"alfa1[i]"<<","<<"T3[i]"<<","<<"P3[i]"<<","<<"v3[i]"<<","<<"rho3[i]"<<","<<"alfa3[i]"<<endl; //alfa, T2,T4[n]=0 perque no hi ha nodes
     for (int i = 0; i < n+1; i++)
     {   
-        fout<<i<<","<<x1[i]<<","<<T1[i]<<","<<p1[i]<<","<<v1[i]<<","<<rho1[i]<<","<<alfa1[i]<<endl;
+        fout<<i<<","<<x1[i]<<","<<T1[i]<<","<<p1[i]<<","<<v1[i]<<","<<rho1[i]<<","<<alfa1[i]<<","<<T3[i]<<","<<p3[i]<<","<<v3[i]<<","<<rho3[i]<<","<<alfa3[i]<<endl;
         
         
     }
@@ -98,18 +119,36 @@ int main(){
 
 
 
-void fluid::Calcul_Coeficients(double mu, double cp, double lambda, double D, double v, double rho, double rugositat_relativa, double x)
+void fluid::Calcul_Coeficients(double mu, double cp, double lambda, double D, double v, double rho, double rugositat_relativa)
 {
     Reynolds=(rho*v*D)/mu;
     Prandt=mu*cp/lambda;
     Nusselt=0.023*pow(Reynolds,0.8)*pow(Prandt,0.4);
-    Alfa_i=lambda*Nusselt/x;
+    Alfa_i=lambda*Nusselt/D;
     double A,B;
     //Churchill expression
     A=pow(2.457*log(1/(pow(7/Reynolds,0.9)+0.27*rugositat_relativa)),16);
     B=pow(37530/Reynolds,16);
     fregament=2*pow(pow(8/Reynolds,12)+1/pow(A+B,1.5),1.0/12.0);
 }
+
+void fluid::Calcul_Coeficients_anular(double mu, double cp, double lambda, double D, double v, double rho, double rugositat_relativa)
+{
+    Reynolds=(rho*v*D)/mu;
+    Prandt=mu*cp/lambda;
+    double A,B;
+    //Churchill expression
+    A=pow(2.457*log(1/(pow(7/Reynolds,0.9)+0.27*rugositat_relativa)),16);
+    B=pow(37530/Reynolds,16);
+    fregament=2*pow(pow(8/Reynolds,12)+1/pow(A+B,1.5),1.0/12.0);
+
+    Nusselt=0.023*pow(Reynolds,0.8)*pow(Prandt,0.4); //Cal mirar quin nusselt, expressio amb Dh o expressio 16 de la seccio C3
+    Alfa_i=lambda*Nusselt/D;
+}
+    
+    
+   
+
 void fluid::Propietats_termofisiquesmescla(double T, double P,double Rgas,double cpA,double cpB,double fmolA,double fmolB, double muA, double muB, double conductivitatA, double conductivitatB){
     densitat=P/(Rgas*T);
     //Suposem que la viscositat, i conductivitat son la mitjana, de moment
