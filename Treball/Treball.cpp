9c9a83aa79c66b7408d94ce75d01025fdc863304
Ferran de Miguel <ferran.de.miguel@estudiantat.upc.edu>
2022-05-29 00:29:44 +0200

    Afegida conveccio natura exterior

--- Treball/Treball.cpp
+++ Treball/Treball.cpp
@@ -9,14 +9,16 @@ auto start = chrono::high_resolution_clock::now();
 class fluid
 {
     private:
-    double Reynolds, Prandt, Nusselt;
+    double Reynolds, Prandt, Nusselt, Grashof, Rayleigh;
     public:
-    double Alfa_i, fregament, viscositat , densitat ,conductivitat ,cp;
+    double Alfa_i, fregament, viscositat , densitat ,conductivitat ,cp , beta;
     void Calcul_Coeficients(double mu, double cp, double lambda, double D, double v, double rho, double rugositat_relativa);
     void Propietats_termofisiquesH2(double T, double P,double Rgas);
     void Propietats_termofisiquesO2(double T, double P,double Rgas);
     void Propietats_termofisiquesmescla(double T, double P,double Rgas,double cpA,double cpB,double fmolA,double fmolB, double muA, double muB, double conductivitatA, double conductivitatB);
     void Calcul_Coeficients_anular(double mu, double cp, double lambda, double D, double v, double rho, double rugositat_relativa);
+    void Propietats_termofisiquesaire(double T, double P,double Rgas);
+    void Calcul_coeficients_exterior(double cp, double lambda, double mu, double g, double beta, double rho, double Tm, double Tf, double X);
 };
 
 
@@ -25,12 +27,13 @@ const double delta = 1e-10;
 const double pi = 2 * acos(0.0);
 const double Runiversal=8.3144621;
 const double massa_molarH2=2e-3, massa_molarO2=32e-3;//Kg/mol
+const double g=9.81;
 
 int main(){
     std::cout.precision(15);
     std::scientific;
     double L=0.5, Di=0.02, Do=0.023, Dm=0.033, DOut=0.035;
-    double Tin1=1000, pin1=161e+5, Tin3=100, cabalin3=0.005, pin3=150e+5, Text=300;
+    double Tin1=1000, pin1=161e+5, Tin3=100, cabalin3=0.005, pin3=150e+5, Text=300, Pext=101325;
     double cabalin1H2=0.005, cabalin1O2=0.05, cabalin1Tot=cabalin1H2+cabalin1O2;
     double Ttub2_inic=800, Ttub4_inic=500;
     double rugositat2in=0.0002, rugositat2ext=0.0003, rugositat4in=0.0004, rugositat4ext=0.0002;
@@ -40,10 +43,10 @@ int main(){
     double S1=pi*Di*Di/4, S3=pi*pow(Dm-Do,2)/4, Sl2int=pi*Di*Deltax, Sl2out=pi*Do*Deltax, Sl4in=pi*Dm*Deltax;
     double molsH2=cabalin1H2/massa_molarH2, molsO2=cabalin1O2/massa_molarO2;
     double fraccio_molarH2=molsH2/(molsH2+molsO2), fraccio_molarO2=molsO2/(molsH2+molsO2), massa_molar_cambra=fraccio_molarH2*massa_molarH2+fraccio_molarO2*massa_molarO2;
-    double Rgas_cambra=Runiversal/massa_molar_cambra, Rhidrogen=Runiversal/massa_molarH2, Roxigen=Runiversal/massa_molarO2;
+    double Rgas_cambra=Runiversal/massa_molar_cambra, Rhidrogen=Runiversal/massa_molarH2, Roxigen=Runiversal/massa_molarO2, Raire=287;
     double rhoin1=pin1/(Tin1*Rgas_cambra), rhoin3=pin3/(Tin3*Rhidrogen);
     double vin1=cabalin1Tot/(S1*rhoin1), vin3=cabalin3/(S3*rhoin3);
-    std::vector<double> x1(n+1,0), v1(n+1,0), T1(n+1,0), p1(n+1,0), rho1(n+1,0), v3(n+1,0),T3(n+1,0), p3(n+1,0), rho3(n+1,0), T2(n,Ttub2_inic), T4(n,Ttub4_inic), alfa1(n,0), alfa3(n,0);
+    std::vector<double> x1(n+1,0), v1(n+1,0), T1(n+1,0), p1(n+1,0), rho1(n+1,0), v3(n+1,0),T3(n+1,0), p3(n+1,0), rho3(n+1,0), T2(n,Ttub2_inic), T4(n,Ttub4_inic), alfa1(n,0), alfa3(n,0), alfa5(n,0);
     x1[0]=0;
     for (int i = 1; i < n+1; i++)
     {
@@ -54,7 +57,7 @@ int main(){
     //Zona 1 i 3
     v1[0]=vin1; T1[0]=Tin1; p1[0]=pin1; rho1[0]=rhoin1;
     v3[0]=vin3; T3[0]=Tin3; p3[0]=pin3; rho3[0]=rhoin3;
-    fluid H2cambra, O2cambra, H2ext, mescla_cambra;
+    fluid H2cambra, O2cambra, H2ext, mescla_cambra, aire;
    
     for (int i=1; i<n+1; i++){
         v1[i]=v1[i-1]; p1[i]=p1[i-1]; T1[i]=T1[i-1]; rho1[i]=rho1[i-1];
@@ -97,7 +100,11 @@ int main(){
             error=max(abs(vold-v3[i]),error);
             error=max(abs(rho3[i]-rhoold),error);
         }
-        
+        //Zona exterior
+        double Tm=(T4[i]+Text)/2;
+        aire.Propietats_termofisiquesaire(Tm,Pext,Raire);
+        aire.Calcul_coeficients_exterior(aire.cp,aire.conductivitat,aire.viscositat,g,aire.beta,aire.densitat,T4[i],Text,DOut);
+        alfa5[i-1]=aire.Alfa_i;
     }
     
     auto stop = chrono::high_resolution_clock::now();
@@ -168,5 +175,26 @@ void fluid::Propietats_termofisiquesH2(double T, double P,double Rgas){
     viscositat= (5.83673e-9*pow(T,3)-1.7564449e-5*pow(T,2)+0.030645686887*T+1.113450152679)*pow(10,-6);
     cp=(-1.468867e-11*pow(T,4)+5.0109515e-8*pow(T,3)-5.914804e-5*pow(T,2)+2.918448e-2*T+9.472783)*pow(10,3);
     conductivitat=3.69347e-15*pow(T,4)+6.18450e-11*pow(T,3)-3.08777e-7*pow(T,2)+6.8004e-4*T+2.8372e-3;
+    
+}
+void fluid::Propietats_termofisiquesaire(double T, double P,double Rgas){
+    densitat=P/(Rgas*T);
+    viscositat=1.458e-6*pow(T,1.5)/(T+110.4); 
+    cp=1034.09-2.849e-1*T+7.817e-4*pow(T,2)-4.971e-7*pow(T,3)+1.077e-10*pow(T,4);
+    conductivitat=(2.648e-3*sqrt(T))/(1+(245.4/T)*pow(10,-12.0/T));
+    beta=1/T;
+}
+void fluid::Calcul_coeficients_exterior(double cp, double lambda, double mu, double g, double beta, double rho, double Tw, double Tf, double X){
+    Prandt=mu*cp/lambda;
+    Grashof=g*beta*pow(rho,2)*abs(Tw-Tf)*pow(X,3)/pow(mu,2);
+    Rayleigh=Grashof*Prandt;
+    if (Rayleigh<10e+9)
+    {
+        Nusselt=0.47*pow(Rayleigh,1.0/4.0);
+    }
+    else{
+        Nusselt=0.1*pow(Rayleigh,1.0/3.0);
+    }
+    Alfa_i=lambda*Nusselt/X;
     
 }
